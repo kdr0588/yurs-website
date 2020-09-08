b7603b58eba7a0dfea44e9579b0c10cffeebe7c7
Ibrahim Abdul Hammed <ibrahim@74115@gmail.com>
2020-09-08 18:52:17 -0400

    update makefile with echo messages

--- publish.bash
+++ publish.bash
@@ -1,6 +1,10 @@
+echo "Initiating PUBLISH script"
+echo "Building website"
 cd website && bundle exec jekyll build
+echo "Website built"
+echo "Initiating transfer to gh_pages repo"
 mv website/_site .
 git checkout gh_pages
 cp -r _site/* .
 rm -rf _site
-
+echo "Transfer complete"
