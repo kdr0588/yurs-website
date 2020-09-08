313d5d6113e09f57ebac32aefb388259c341841d
Ibrahim Abdul Hammed <ibrahim@74115@gmail.com>
2020-09-08 19:06:14 -0400

    update script

--- publish.bash
+++ publish.bash
@@ -1,3 +1,6 @@
+# constants
+DEFAULT_MSG = "New changes to website."
+
 echo "Initiating PUBLISH script"
 echo "Building website"
 cd website && bundle exec jekyll build && cd ..
@@ -8,3 +11,16 @@ git checkout gh_pages
 cp -r _site/* .
 rm -rf _site website
 echo "Transfer complete"
+echo "Initiating publish"
+
+read -p "Please enter your msg: " msg
+
+if [[ -z "$msg" ]];
+then
+    echo "No message was input, resorting to default msg: $DEFAULT_MSG"
+    $msg = $DEFAULT_MSG
+else
+    echo "msg is not empty, using '$msg'"
+fi
+
+echo "commit -m $msg"
