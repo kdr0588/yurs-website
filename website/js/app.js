02aaae7f536711a61e62ab6d0c63786360a8a8fb
Ibrahim Abdul Hammed <ibrahim@74115@gmail.com>
2020-09-11 21:24:56 -0400

    Remove unneeded files

--- website/js/app.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links-responsive");
-const links = document.querySelectorAll(".nav-link-responsive");
-
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-});
